3f3d55ec0a4f0ae764a5b9dce9c30ddb05c529e3
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-10 20:31:34 +0100

    lo pongo todo bien para que funcione, lo he ejecutado y funciona, nos ponemos en marcha con la siguiente ficha del tablero

--- ajedrez.py
+++ ajedrez.py
@@ -9,12 +9,14 @@ def tablero():
     tablero = []
     for i in range(6):
         fila = []
-    for j in range(8):
-        fila.append('X')
-        if tuple[(i,j)] in peon:
-            fila.append('p')
+        for j in range(8):
+            if tuple ([i,j]) in peon:
+                fila.append('p')
+            else:
+                fila.append('x')
         tablero.append(fila)
     return tablero
+  
 
 peon = ((1,0), (1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (4,0), (4,1), (4,2), (4,3), (4,4), (4,5), (4,6), (4,7))
 tab = tablero()
