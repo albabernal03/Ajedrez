3fc41718e296c72cc2860f0495c27c6226affb46
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-10 20:01:30 +0100

    definimos las columnas

--- ajedrez.py
+++ ajedrez.py
@@ -1,4 +1,9 @@
 def tablero():
     tablero = []
-    for i in range (6):
+    for i in range(6):
         fila = []
+    for j in range(8):
+        fila.append('X')
+        tablero.append(fila)
+    return tablero
+
