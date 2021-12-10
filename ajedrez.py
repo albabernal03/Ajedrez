7f45e6b3d0dba00873b3aa123d6a05c7d22003fc
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-10 20:19:03 +0100

    defino la variable peon (estableciendo sus coordenadas)

--- ajedrez.py
+++ ajedrez.py
@@ -14,6 +14,7 @@ def tablero():
         tablero.append(fila)
     return tablero
 
+peon = ((1,0), (1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (4,0), (4,1), (4,2), (4,3), (4,4), (4,5), (4,6), (4,7))
 tab = tablero()
 
 for i in tab:
