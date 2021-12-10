dd37ffccbeb1c52cb3e0ebb58f9c7553fdf34e47
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-10 20:44:40 +0100

    creo la variable canallo y continuamos en el mismo método

--- ajedrez.py
+++ ajedrez.py
@@ -16,6 +16,8 @@ def tablero():
                 fila.append('t')
             elif tuple ([i,j]) in alfil:
                 fila.append('a')
+            elif tuple ([i,j]) in caballo:
+                fila.append('c')
             else:
                 fila.append('x')
         tablero.append(fila)
@@ -25,6 +27,7 @@ def tablero():
 peon = ((1,0), (1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (4,0), (4,1), (4,2), (4,3), (4,4), (4,5), (4,6), (4,7))
 torre = ((0,0), (0,7), (5,0), (5,7))
 alfil = ((0,2), (0,5), (5,2), (5,5))
+caballo = ((0,1), (0,6), (5,1), (5,6))
 tab = tablero()
 
 for i in tab:
